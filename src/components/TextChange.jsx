7d0efe590e7b336a4f0fcb8ccb5ecb4a0e6f764a
YUVRAAJ SINGH <singhyuvi0911@gmail.com>
2024-09-16 12:53:12 +0530

    TextChange.jsx

--- src/components/TextChange.jsx
+++ src/components/TextChange.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 const TextChange = () => {
-  const texts = ["Hi, I'm Yuvi...", "Hi, I'm Yuvi...", "Hi, I'm Yuvi..."];
+  const texts = ["Hi, I'm Yuvi", "Hi, I'm Yuvi", "Hi, I'm Yuvi"];
   const [currenText, setCurrentText] = useState("");
   const [endValue, setendValue] = useState(true);
   const [isForward, setIsForward] = useState(true);
