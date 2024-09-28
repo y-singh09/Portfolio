1f9dc3a35a0f00ff6446375e931f4e6f6529e272
YUVRAAJ SINGH <singhyuvi0911@gmail.com>
2024-09-28 17:51:50 +0530

    Certifications.jsx

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -5,106 +5,116 @@ const Certifications = () => {
   return (
     <div
       id="certificate"
-      className="text-black md:flex overflow-hidden items-center md:flex-wrap md:justify-center bg-[url('https://image.slidesdocs.com/responsive-images/docs/graduation-with-a-business-certificate-page-border-background-word-template_2e488355a5__1131_1600.jpg')] bg-center bg-cover rounded-lg py-10"
+      className="text-white md:flex overflow-hidden items-center md:flex-wrap md:justify-center bg-[url('https://img.freepik.com/free-vector/gradient-dynamic-purple-lines-background_23-2148995757.jpg?w=900&t=st=1726312493~exp=1726313093~hmac=42c51de277b695a4db8bc29bfec4fb7ae964095ea148998a617db72684646c1b')] bg-center bg-cover rounded-lg py-10"
     >
       <div>
-        <h2 className="text-2xl text-white md:text-4xl font-bold text-center underline">Certifications and Achievments</h2>
-        <div className="md:flex flex-wrap flex-col md:flex-row items-center">
+        <h2 className="text-2xl text-white md:text-4xl font-bold text-center underline">Certifications and Achievements</h2>
+        <div className="md:flex my-10 flex-wrap flex-col md:flex-row items-center">
           <ul>
-          <div className="flex gap-3 py-4">
-  <IoArrowForward size={55}className="mt-1 " />
-  <span className="w-120 ">
-    <h1 className="text-xl text-blue-600 py-4 md:text-2xl font-semibold leading-normal hover:text-purple-900 underline ">
-      <a href="https://drive.google.com/file/d/1Z5MFYd2ndBviZ0dhhtI9jMIAFuwyLCEd/view?usp=drive_link">
-      DSA using Java
-      </a>
-    </h1>
-    <img
-     className="md:h-80 px-80 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:opacity-90 "
-      src="https://i.imgur.com/i7DGyE3.png"
-      alt="About img"
-    />
-    <p className="text-md py-7 md:text-md leading-tight">
-      Data Structures and Algorithms (DSA) is a fundamental concept in computer science that helps solve complex problems
-      efficiently by <br/>organizing data and optimizing operations. Java is a popular language for implementing DSA due to its
-      object-oriented nature and rich libraries.<br/> Java's collections framework (e.g., ArrayList, HashMap, Stack) makes it easy to
-      implement these structures. DSA is crucial for writing optimized code, reducing time and space complexity, and solving
-      real-world problems effectively.<br/>
-    </p>
-  </span>
-</div>
+            <div className="flex flex-col my-10 md:flex-row gap-3 py-4 items-center">
+      
+              <div className="flex-shrink-0">
+              <a href="https://drive.google.com/file/d/1Z5MFYd2ndBviZ0dhhtI9jMIAFuwyLCEd/view?usp=drive_link" target="_blank" className="text-xl text-blue-600  md:text-2xl font-semibold leading-normal hover:text-purple-900 underline">
 
-            <div className="flex gap-3 py-4">
-              <IoArrowForward size={55}className="mt-1" />
-
-              <span className="w-120">
-                <h1 className="text-xl text-blue-600 py-4 md:text-2xl font-semibold leading-normal hover:text-purple-900 underline">
-                  <a href="https://drive.google.com/file/d/1Z8JDLCnkNZYwDoQypZaV7ygqiHF7yutj/view?usp=drive_link">
-                  Core Java Concepts</a>
-                </h1>
-                <img className="md:h-80 px-80 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:opacity-90" src='https://i.imgur.com/O2gS07A.png' alt="About img" />
-                <p className="text-md py-5 md:text-md leading-tight">
-                Core Java focuses on the foundational concepts of the Java programming language, including Object-Oriented Programming (OOP) principles<br/> like inheritance, polymorphism, encapsulation, and abstraction. 
-                <br/> Other Advance Concepts such as exception handling, and multithreading for concurrent processing. 
-               <br/> Additionally, it introduces Java's memory management and the Collections framework, which provides data structures like ArrayList, HashMap, and LinkedList.
-                </p>
-              </span>
-            </div>
-            <div className="flex gap-3 py-4">
-              <IoArrowForward size={55}className="mt-1" />
-
-              <span className="w-120">
-                <h1 className="text-xl text-blue-600 py-4 :text-2xl font-semibold leading-normal hover:text-purple-900 underline">
-                  <a href="https://drive.google.com/file/d/1MLol6k8NTSy_XaOcUhsch0z11WbVUZXk/view?usp=drive_link">
-                 DBMS and Sql
-                 </a>
-                </h1>
                 <img
-  className="md:h-80 px-80 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:opacity-90 "
-  src='https://i.imgur.com/JguIsL8.png'
-  alt="About img"
-/>
-
-
-                <p className="text-md md:text-md py-5 leading-tight">
-                A Database Management System (DBMS) is software that enables the creation, management, and manipulation of databases, allowing users to store and retrieve data efficiently.<br/> 
-                SQL (Structured Query Language) is the standard language used to interact with relational databases, performing operations like querying, updating, and managing data.
-                <br/>Key SQL commands include SELECT, INSERT, UPDATE, DELETE, and JOIN, which are used to handle and manipulate data within a DBMS.
+                  className="md:h-60 px-5 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:opacity-90"
+                  src="https://i.imgur.com/i7DGyE3.png"
+                  alt="DSA using Java"
+                />
+                </a>
+              </div>
+              
+              <div className="flex flex-col  ">
+                <a href="https://drive.google.com/file/d/1Z5MFYd2ndBviZ0dhhtI9jMIAFuwyLCEd/view?usp=drive_link" className="text-xl text-blue-600  md:text-2xl font-semibold leading-normal hover:text-purple-900 underline">
+                  DSA using Java
+                </a>
+                <p className="text-md py-7 md:text-md leading-tight">
+                  Data Structures and Algorithms (DSA) is a fundamental concept in computer science that helps solve complex problems
+                  efficiently by organizing data and optimizing operations. Java is a popular language for implementing DSA due to its
+                  object-oriented nature and rich libraries. Java's collections framework (e.g., ArrayList, HashMap, Stack) makes it easy to
+                  implement these structures. DSA is crucial for writing optimized code, reducing time and space complexity, and solving
+                  real-world problems effectively.
                 </p>
-              </span>
+              </div>
             </div>
-            <div className="flex gap-3 py-4">
-              <IoArrowForward size={55}className="mt-1" />
 
-              <span className="w-120">
-                <h1 className="text-xl text-blue-600 py-4 :text-2xl font-semibold leading-normal hover:text-purple-900 underline">
-                 Leetcode Badges
+            <div className="flex flex-col md:flex-row my-10 gap-3 py-4 items-center">
+           
+              <div className="flex-shrink-0">
+              <a href="https://drive.google.com/file/d/1Z8JDLCnkNZYwDoQypZaV7ygqiHF7yutj/view?usp=drive_link" target="_blank" className="text-xl text-blue-600 py-4 md:text-2xl font-semibold leading-normal hover:text-purple-900 underline">
+
+                <img
+                  className="md:h-60 px-5  transition-transform duration-300 ease-in-out transform hover:scale-105 hover:opacity-90"
+                  src="https://i.imgur.com/O2gS07A.png"
+                  alt="Core Java Concepts"
+                />
+                </a>
+              </div>
+              <div className="flex flex-col">
+                <a href="https://drive.google.com/file/d/1Z8JDLCnkNZYwDoQypZaV7ygqiHF7yutj/view?usp=drive_link" className="text-xl text-blue-600 py-4 md:text-2xl font-semibold leading-normal hover:text-purple-900 underline">
+                  Core Java Concepts
+                </a>
+                <p className="text-md py-5 md:text-md leading-tight">
+                  Core Java focuses on the foundational concepts of the Java programming language, including Object-Oriented Programming (OOP) principles
+                  like inheritance, polymorphism, encapsulation, and abstraction. Other advanced concepts such as exception handling, and multithreading for concurrent processing. Additionally, it introduces Java's memory management and the Collections framework, which provides data structures like ArrayList, HashMap, and LinkedList.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-col md:flex-row gap-3  py-4 items-center">
+       
+              <div className="flex-shrink-0">
+              <a href="https://drive.google.com/file/d/1MLol6k8NTSy_XaOcUhsch0z11WbVUZXk/view?usp=drive_link" target="_blank" className="text-xl text-blue-600 py-4 md:text-2xl font-semibold leading-normal hover:text-purple-900 underline">
+
+                <img
+                  className="md:h-60 px-5  transition-transform duration-300 ease-in-out transform hover:scale-105 hover:opacity-90"
+                  src="https://i.imgur.com/JguIsL8.png"
+                  alt="DBMS and SQL"
+                />
+                </a>
+              </div>
+              
+              <div className="flex flex-col">
+                <a href="https://drive.google.com/file/d/1MLol6k8NTSy_XaOcUhsch0z11WbVUZXk/view?usp=drive_link" className="text-xl text-blue-600 py-4 md:text-2xl font-semibold leading-normal hover:text-purple-900 underline">
+                  DBMS and SQL
+                </a>
+                <p className="text-md py-5 md:text-md leading-tight">
+                  A Database Management System (DBMS) is software that enables the creation, management, and manipulation of databases, allowing users to store and retrieve data efficiently. SQL (Structured Query Language) is the standard language used to interact with relational databases, performing operations like querying, updating, and managing data.
+                  Key SQL commands include SELECT, INSERT, UPDATE, DELETE, and JOIN, which are used to handle and manipulate data within a DBMS.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-col md:flex-row gap-3 py-4 items-center">
+              <div className="flex-shrink-0">
+                
+              </div>
+              <div className="flex flex-col">
+              <h1 className="text-xl text-center text-blue-600 my-10 py-6 md:text-2xl font-semibold leading-normal hover:text-purple-900 underline">
+                  Leetcode Badges
                 </h1>
                 <div className="flex flex-wrap justify-center gap-4">
-  <img
-    className="md:h-80 px-2"
-    src='https://assets.leetcode.com/static_assets/marketing/2024-50.gif'
-    alt="Leetcode Badge 2024-50"
-  />
-  <img
-    className="md:h-80 px-2"
-    src='https://leetcode.com/static/images/badges/2024/gif/2024-06.gif'
-    alt="Leetcode Badge 2024-06"
-  />
-  <img
-    className="md:h-80 px-2"
-    src='https://leetcode.com/static/images/badges/2024/gif/2024-07.gif'
-    alt="Leetcode Badge 2024-07"
-  />
-</div>
-
-                <p className="text-md md:text-md py-5 leading-tight text-blue-900">
-                Leetcode Badges are visual representations of a user's achievements on the Leetcode platform. 
-                These badges are awarded for reaching milestones such as solving a specific number of problems, achieving a certain ranking, or participating in contests. <br/>
-                They serve as a recognition of problem-solving skills and coding proficiency, motivating users to track their progress and engage with the platform more actively.<br/> 
-                Leetcode badges can be showcased in profiles, resumes, or shared on social platforms to highlight expertise in competitive programming and algorithmic challenges.<br/>
+                  <img
+                    className="md:h-80 px-2"
+                    src="https://assets.leetcode.com/static_assets/marketing/2024-50.gif"
+                    alt="Leetcode Badge 2024-50"
+                  />
+                  <img
+                    className="md:h-80 px-2"
+                    src="https://leetcode.com/static/images/badges/2024/gif/2024-06.gif"
+                    alt="Leetcode Badge 2024-06"
+                  />
+                  <img
+                    className="md:h-80 px-2"
+                    src="https://leetcode.com/static/images/badges/2024/gif/2024-07.gif"
+                    alt="Leetcode Badge 2024-07"
+                  />
+                </div>
+               
+                <p className="text-md  md:text-md leading-tight text-white">
+                  Leetcode Badges are visual representations of a user's achievements on the Leetcode platform. These badges are awarded for reaching milestones such as solving a specific number of problems, achieving a certain ranking, or participating in contests. They serve as a recognition of problem-solving skills and coding proficiency, motivating users to track their progress and engage with the platform more actively. Leetcode badges can be showcased in profiles, resumes, or shared on social platforms to highlight expertise in competitive programming and algorithmic challenges.
                 </p>
-              </span>
+              </div>
             </div>
           </ul>
         </div>
