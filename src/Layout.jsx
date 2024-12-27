2f92faac81c021eb2a6b378d4a7f7f2f5fdf6b1b
YUVRAAJ SINGH <singhyuvi0911@gmail.com>
2024-12-27 20:35:13 +0530

    update layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/Header/Header.jsx'
-import Footer from './Components/Footer/Footer.jsx'
+import Footer from './components/Footer/Footer.jsx'
 import { Outlet } from 'react-router-dom'
 function Layout() {
   return (
